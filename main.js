8bc8b7915ab012b119af733cb80d966fa275e8ab
candyflipprog <ss494120@gmail.com>
2024-05-30 10:56:37 +0300

    two

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 import { functionLi } from "./title.js"
-let funOtr = async () => {
-    const all1 = await fetch("https://gorest.co.in/public/v2/posts")
+let funOtr = async (id) => {
+    const all1 = await fetch(`https://gorest.co.in/public/v2/posts?page=${id}`)
     let all = await all1.json();
     console.log(all)
-    await functionLi(all)
+    functionLi(all)
 }
-
-funOtr()
+funOtr(10)
 
 
